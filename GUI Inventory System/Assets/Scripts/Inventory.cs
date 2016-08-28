0e33ea2f47f4e1a47a8370dab12a3477f22b70b0
WishPotato <wish92jfn@hotmail.com>
2016-08-28 15:31:19 +0200

    Updated inventory
    Easier removal and adding of items.
    Added description of how to use it.
    Added mouse-over description of item.

---
+++ GUI Inventory System/Assets/Scripts/Inventory.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+
+    private ItemDatabase itemDatabase;
+    private Item[] inventory;
+
+    private bool hasOpen = true;
+
+    private int slotAmountX = 8;
+    private int slotAmountY = 8;
+    private int ItemSlot = 0;
+
+    private float slotSize = 64.0f;
+    private float slotSpacing = 10.0f;
+
+    private string hoverText = "";
+
+    private Rect inventoryBorder;
+
+	void Start () 
+    {
+        itemDatabase = GameObject.Find("Database").GetComponent<ItemDatabase>();
+        
+        inventory = new Item[64];
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            inventory[i] = itemDatabase.dataBase[0];
+        }
+        inventoryBorder = new Rect(10, 10, (slotSize + slotSpacing) * (slotAmountX + 5), (slotSize + slotSpacing) * slotAmountY + slotSpacing);
+
+    }
+	
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            hasOpen = !hasOpen;
+
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Alpha1))
+            RemoveItem(1);
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+            AddItem(1);
+
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Alpha2))
+            RemoveItem(2);
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+            AddItem(2);
+
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Alpha3))
+            RemoveItem(3);
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+            AddItem(3);
+    }
+
+    void OnGUI()
+    {
+        if (hasOpen)
+        {
+            // Event.current.mousePosition;
+            GUI.Box(inventoryBorder, "");
+            GUI.Label(new Rect(inventoryBorder.x + 600, inventoryBorder.y + 100, 500, 200), "Add a Item to the inventory by clicking 1, 2, or 3. \n\nRemove a Item from the inventory by Left Shift + 1, 2 or 3 \n\nHover the mouse over a item to read its description");
+            GUI.Label(new Rect(inventoryBorder.x + 600, inventoryBorder.y + 300, 500, 500), hoverText);
+            int count = 0;
+            for (int i = 0; i < slotAmountY; i++)
+            {
+                for (int j = 0; j < slotAmountX; j++)
+                {
+                    Rect slotBorder = new Rect((inventoryBorder.x + slotSize) * j + slotSpacing * 2, (inventoryBorder.y + slotSize) * i + slotSpacing * 2, slotSize, slotSize);
+                    GUI.Box(slotBorder, inventory[count].Name + " " + inventory[count].Stack);
+                    if (slotBorder.Contains(Event.current.mousePosition) && inventory[count].ID != 0)
+                        hoverText = inventory[count].Description;
+                    count++;
+                    
+                }
+            }
+        }
+    }
+
+    void AddItem(int id)
+    {
+        if (itemDatabase.dataBase.Count < id || id == 0)
+            Debug.Log("ERROR!: Item ID does not exsist within the field of the database.");
+        else if (ItemIsInSlot(id))
+            inventory[ItemSlot].Stack++;
+        else
+        {
+            for (int i = 0; i < inventory.Length; i++)
+            {
+                if (inventory[i].ID == 0)
+                {
+                    inventory[i] = itemDatabase.dataBase[id];
+                    inventory[i].Stack = 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    private void RemoveItemFromSlot(int slot)
+    {
+        if (inventory[slot].ID == 0)
+            Debug.Log("Slot is empty already!");
+        else if (inventory[slot].Stack > 1)
+            inventory[slot].Stack--;
+        else
+            inventory[slot] = itemDatabase.dataBase[0];
+    }
+
+    void RemoveItem(int id)
+    {
+        if (ItemIsInSlot(id))
+            RemoveItemFromSlot(ItemSlot);
+    }
+
+    private bool ItemIsInSlot(int id)
+    {
+        bool isInSlot = false;
+
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            if(inventory[i].ID == id)
+            {
+                isInSlot = true;
+                ItemSlot = i;
+                break;
+            }
+        }
+        return isInSlot;
+    }
+
+}
--- GUI Inventory System/Assets/Scripts/ItemDatabase.cs
+++ GUI Inventory System/Assets/Scripts/ItemDatabase.cs
@@ -7,10 +7,11 @@ public class ItemDatabase : MonoBehaviour {
 
     public List<Item> dataBase = new List<Item>();
 
-	void Start () {
-        dataBase.Add(new Item());
-        dataBase.Add(new Item(1, "Key", "Key to The Great Door."));
-        dataBase.Add(new Item(2, "Old Apple", "This apple should not be eaten."));
+	void Awake () {
+        dataBase.Add(new Item(0, "empty", "N/A", 0));
+        dataBase.Add(new Item(1, "Key", "Key to The Great Door.", 0));
+        dataBase.Add(new Item(2, "Apple", "This apple should not be eaten.", 0));
+        dataBase.Add(new Item(3, "Dust", "The Dust of a person who walked upon it.", 0));
 	}
 	
 }
@@ -20,19 +21,14 @@ public class Item
     private string name;
     private string description;
     private int Id;
+    private byte stack;
 
-    public Item(int id, string n, string d)
+    public Item(int id, string n, string d, byte s)
     {
         Id = id;
         name = n;
         description = d;
-    }
-
-    public Item()
-    {
-        Id = 0;
-        name = "Empty";
-        description = "N/A";
+        stack = s;
     }
 
     public string Name
@@ -50,5 +46,12 @@ public class Item
     public int ID
     {
         get { return Id; }
+        set { Id = value; }
+    }
+
+    public byte Stack
+    {
+        get { return stack; }
+        set { stack = value; }
     }
 }
